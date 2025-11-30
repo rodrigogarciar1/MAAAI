343ea446c7db6ffb062fba1a1a5af4e3c878baec
Abl05 <Abl05>
2025-11-30 20:24:44 +0100

    inicios preprocesado

--- PracticaFinal/P2.jl
+++ PracticaFinal/P2.jl
@@ -1,3 +1,17 @@
-function test()
-    print("Hello world")
+using CSV
+using DataFrames
+
+function unifyDataset(folderName, fileName)
+    fileData = DataFrame()
+    for (path, _, files) in walkdir(folderName)
+        for file in files
+            fileData = vcat(fileData, CSV.read(joinpath(path, file), DataFrame), cols= :union)
+        end
+    end
+    sort!(fileData)
+    CSV.write(fileName, fileData)
+end
+
+function separateDataframe(df)
+    return df[:,1], df[:,2:end-1], df[:,end]
 end
